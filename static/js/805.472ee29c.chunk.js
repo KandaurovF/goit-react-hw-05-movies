0c541cfbe04f72b90412298cdc5b533674a45ac1
KandaurovF <122234649+KandaurovF@users.noreply.github.com>
2023-08-20 05:52:43 +0000

    Deploying to gh-pages from @ KandaurovF/goit-react-hw-05-movies@e7b05a970aef592f2ee862a143dedd5c9eff7d56 🚀

--- static/js/805.472ee29c.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{805:function(e,t,r){r.r(t);r(791);var n=r(87),c=r(184);t.default=function(){return(0,c.jsxs)(c.Fragment,{children:[(0,c.jsx)("h1",{children:"Not found. 404"}),(0,c.jsx)(n.rU,{to:"/",children:"Home"})]})}}}]);
-//# sourceMappingURL=805.472ee29c.chunk.js.map
---
+++ static/js/805.63f97faf.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{9805:function(e,t,r){r.r(t);r(2791);var n=r(1087),c=r(184);t.default=function(){return(0,c.jsxs)(c.Fragment,{children:[(0,c.jsx)("h1",{children:"Not found. 404"}),(0,c.jsx)(n.rU,{to:"/",children:"Home"})]})}}}]);
+//# sourceMappingURL=805.63f97faf.chunk.js.map
